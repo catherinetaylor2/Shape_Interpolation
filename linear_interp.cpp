a9eea284ecbc56e70827dfddaa68f2c116f78da8
Catherine <cat.taylor.taylor@btinternet.com>
2017-04-27 20:20:57 +0100

    Initial commit.

---
+++ linear_interp.cpp
@@ -0,0 +1,14 @@
+#include <glew.h>
+#include <glfw3.h>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdio>
+ #include <glm/glm.hpp>
+
+ int main(){
+
+     return 0;
+ }
