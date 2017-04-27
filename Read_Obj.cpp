72111073c2ca36f09924d0608eb351e47b3ac087
Catherine <cat.taylor.taylor@btinternet.com>
2017-04-27 21:01:00 +0100

    Input mesh now draws.

--- Read_Obj.cpp
+++ Read_Obj.cpp
@@ -41,7 +41,7 @@ void ObjFile::get_vertices(float** V){
     s = str;  
   }
   for(int i=0; i<3*number_of_vertices; i+=3){
-    (*V)[i] = -f1;
+    (*V)[i] = f1;
     (*V)[i+1] = f2;
     (*V)[i+2]=f3; 
     t = fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
@@ -76,7 +76,7 @@ void ObjFile::get_normals(float** N){
     s = str;  
   }
   for(int i=0; i<3*number_of_normals; i+=3){
-    (*N)[i] = -f1;
+    (*N)[i] = f1;
     (*N)[i+1] = f2;
     (*N)[i+2]=f3;   
     fscanf(myObject, "%s %f %f %f" , str, &f1, &f2, &f3);
--- linear_interp.cpp
+++ linear_interp.cpp
@@ -93,8 +93,56 @@
         indices[i+2]=FV[i+2]-1;   
     }
 
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, 3*number_of_vertices*sizeof(float),  &vertices[0], GL_DYNAMIC_DRAW);
+
+    GLuint IBO;
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3*number_of_faces*sizeof(unsigned int), indices, GL_DYNAMIC_DRAW); 
+
+    while(!glfwWindowShouldClose(window)){
+
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Use  shader
+        glUseProgram(programID);
+        glUniformMatrix4fv(Mvp, 1, GL_FALSE, &MVP[0][0]);
+
+        //  1rst attribute buffer : vertices. Including elements buffer with vertex positions.
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //draws the triangles correctly as lines.
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+        GLint posAttrib = glGetAttribLocation(programID, "position");
+        glEnableVertexAttribArray(posAttrib);
+        glVertexAttribPointer(0, // 0  is vertex
+                             3, //size of information
+                             GL_FLOAT, // type of the data
+                             GL_FALSE, // normalised?
+                             0, // stride
+                             0 // offset
+        );        
+        glDrawElements(GL_TRIANGLES, 3*number_of_faces,  GL_UNSIGNED_INT,0); // draw mesh
+
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+        glfwWindowShouldClose(window) == 0 );
 
     glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &IBO);    
+
     delete V;
     delete N;
     delete VT;
