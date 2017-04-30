2f9d012ebe0385a6d9fee1de2cef0b6c6c0b3ae3
Catherine <cat.taylor.taylor@btinternet.com>
2017-04-30 17:49:44 +0100

    Implemented paper for fixed t.

--- rigid_interp.cpp
+++ rigid_interp.cpp
@@ -6,12 +6,15 @@
 #include <cmath>
 #include <string>
 #include <cstdio>
+#include <vector>
 #include <glm/glm.hpp>
 #include "shader.hpp"
 #include <glm/gtx/transform.hpp>
 #include <Eigen/Eigen/Dense>
 #include "read_Obj.hpp"
 #include <omp.h> 
+
+int t=0;
 
 int main(){
     ObjFile mesh("dino2.obj"); // load mesh information from object file.
@@ -39,6 +42,8 @@ int main(){
     }
 
     float scale = 0.2;
+
+    float* V_intermediate = new float[3*number_of_vertices];
 
 
 
@@ -119,6 +124,50 @@ int main(){
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3*number_of_faces*sizeof(unsigned int), indices, GL_DYNAMIC_DRAW); 
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//ALGORITHM:
+
+    Eigen::MatrixXf ideal_affine(2,2), P(6,6), Q(6,1), A_temp, G, I(2,2);
+    std::vector<Eigen::MatrixXf> Affine_transforms;
+    std::vector<Eigen::MatrixXf> inverse_matrices;
+    int index_1, index_2, index_3;
+    for (int i=0; i<number_of_faces; i++){
+        index_1 = FV[3*i]-1, index_2 = FV[3*i+1]-1, index_3 = FV[3*i+2]-1;
+        P<< V[3*index_1],V[3*index_1+1], 1, 0, 0, 0,
+            0,0,0, V[3*index_1],V[3*index_1+1], 1,
+            V[3*index_2], V[3*index_2 +1], 1, 0,0,0,
+            0,0,0, V[3*index_2], V[3*index_2 +1], 1,
+            V[3*index_3], V[3*index_3+1],1, 0,0,0,
+            0,0,0,V[3*index_3], V[3*index_3+1],1;
+
+        Q<< V2[3*index_1],
+            V2[3*index_1+1],
+            V2[3*index_2],
+            V2[3*index_2 +1],
+            V2[3*index_3],
+            V2[3*index_3+1];
+
+        A_temp = (P.inverse())*Q;
+
+        ideal_affine(0,0) = A_temp(0,0);
+        ideal_affine(0,1) = A_temp(1,0);
+        ideal_affine(1,0) = A_temp(3,0);
+        ideal_affine(1,1) = A_temp(4,0);
+// std::cout<<"ideal "<<ideal_affine<<"\n";
+        inverse_matrices.push_back(P.inverse());
+        Affine_transforms.push_back(ideal_affine);
+    }
+    I<<1,0,
+        0,1;
+
+        Eigen::JacobiSVD<Eigen::MatrixXf> svd(Affine_transforms[5],  Eigen::ComputeFullU | Eigen::ComputeFullV);
+        std::cout<<"A "<<Affine_transforms[5]<<"\n";
+        std::cout<<"U "<<svd.matrixU() <<"\n";
+        std::cout<<"V "<<svd.matrixV()<<"\n";
+        std::cout<<"D"<<svd.singularValues()<<"\n";
+        std::cout<<"inv_px"<<inverse_matrices[5]<<"\n";
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
     while(!glfwWindowShouldClose(window)){    
 
         // Clear the screen
@@ -145,6 +194,68 @@ int main(){
         );        
         glDrawElements(GL_TRIANGLES, 3*number_of_faces,  GL_UNSIGNED_INT,0); // draw mesh
 
+//------------------------------------------------------------------------------------------------------------------------------
+//ALGORITHM:
+       Eigen::MatrixXf U, D(2,2), Vm, Rot, Rot_t(2,2), symmetric, A_t, A(4*number_of_faces+2, 2*number_of_vertices), b(4*number_of_faces+2,1), V_new;
+        for(int i=0; i<number_of_faces; i++){
+
+            Eigen::JacobiSVD<Eigen::MatrixXf> svd(Affine_transforms[i],  Eigen::ComputeThinU | Eigen::ComputeThinV);
+            Vm = svd.matrixU();
+            Vm(0,0)=-Vm(0,0);
+            Vm(1,0)=-Vm(1,0);
+            
+            U= svd.matrixV();
+            U(0,0)=-U(0,0);
+            U(1,0)=-U(1,0);
+
+            D<<  svd.singularValues()[0], 0,
+                0,  svd.singularValues()[1];
+
+            symmetric = U*D*(U.transpose());
+            Rot = Vm*U.transpose();
+            
+            Rot_t<< (Rot(0,0)-1)*t + 1, Rot(0,1)*t,
+                    Rot(1,0)*t, (Rot(1,1)-1)*t+1;
+
+           A_t = Rot_t*((1-t)*I+t*symmetric);
+
+           b(4*i) = A_t(0,0);
+           b(4*i+1) = A_t(0,1);
+           b(4*i+2) = A_t(1,0);
+           b(4*i+3) = A_t(1,1);
+
+           for(int k=0; k<3; k++){
+                A(4*i, 2*(FV[3*i+k]-1)) = (inverse_matrices[i])(0, 2*k);
+                A(4*i, 2*(FV[3*i+k]-1)+1) = (inverse_matrices[i])(0, 2*k+1);
+                A(4*i+1, 2*(FV[3*i+k]-1)) = (inverse_matrices[i])(1, 2*k);
+                A(4*i+1, 2*(FV[3*i+k]-1)+1) = (inverse_matrices[i])(1, 2*k+1);
+                A(4*i+2, 2*(FV[3*i+k]-1)) = (inverse_matrices[i])(3, 2*k);
+                A(4*i+2, 2*(FV[3*i+k]-1)+1) = (inverse_matrices[i])(3, 2*k+1);
+                A(4*i+3, 2*(FV[3*i+k]-1)) = (inverse_matrices[i])(4, 2*k);
+                A(4*i+3, 2*(FV[3*i+k]-1)+1) = (inverse_matrices[i])(4, 2*k+1);
+           }
+        }
+
+        b(4*number_of_faces)= (1-t)*(V[0])+t*V2[0];  
+        b(4*number_of_faces+1)= (1-t)*V[1]+t*V2[1];
+
+        A(4*number_of_faces,0) = 1;
+        A(4*number_of_faces+1,1)=1;
+
+        V_new = ((A.transpose()*A).inverse())*A.transpose()*b; //(A.transpose()*A).llt().solve(A.transpose()*b); //
+
+        for (int i=0; i<number_of_vertices; i++){
+                    V_intermediate[3*i] = V_new(2*i,0);
+                    V_intermediate[3*i + 1]= V_new(2*i+1,0);
+                    V_intermediate[3*i + 2] = 0;
+        }
+std::cout<<"b "<<b<<"\n";
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glBufferSubData(GL_ARRAY_BUFFER, 0,  3*number_of_vertices*sizeof(float), &V_intermediate[0]);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+ //------------------------------------------------------------------------------------------------------------------------------       
+
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
         glfwSwapBuffers(window);
@@ -162,6 +273,7 @@ int main(){
     mesh_2.clean_up(V2, N2, VT2, FV2, FN2, F_VT2);
     delete[] vertices;
     delete[] indices;
+    delete[] V_intermediate;
 
     return 0;
 }
