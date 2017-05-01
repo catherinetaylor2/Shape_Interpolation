82f41a267d621eb6e7ed1ba8b33e7ec6bb0ca8cb
Catherine <cat.taylor.taylor@btinternet.com>
2017-05-01 14:58:16 +0100

    Began 3D lerp in C++.

---
+++ lerp_3D.cpp
@@ -0,0 +1,20 @@
+#include <glew.h>
+#include <glfw3.h>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <glm/glm.hpp>
+#include "shader.hpp"
+#include <glm/gtx/transform.hpp>
+#include <Eigen/Eigen/Dense>
+#include "read_Obj.hpp"
+#include <omp.h> 
+
+int main(){
+
+    return 0;
+}
