0bdbdd78c419920abed7961daa5e3f7d0016f738
Catherine <cat.taylor.taylor@btinternet.com>
2017-05-05 16:21:10 +0100

    Made pineapple mesh.

--- rigid_interp_3D.cpp
+++ rigid_interp_3D.cpp
@@ -49,7 +49,7 @@ int main(){
     int number_of_faces = mesh.get_number_of_faces();
     int number_of_vertices = mesh.get_number_of_vertices();
   
-    ObjFile mesh_2("sphere_2.obj");
+    ObjFile mesh_2("sphere_3.obj");
     float* V2 , *N2, *VT2;
     int *FV2, *FN2, *F_VT2;
     mesh_2.get_vertices(&V2);
@@ -145,111 +145,111 @@ int main(){
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //ALGORITHM :
 
-Eigen::Vector3f v1, v2, v3, v4, v1_v2, v2_v3, cross_v, u1, u2, u3, u4, u1_u2, u2_u3, cross_u, T ;
-Eigen::MatrixXf V_t(3,3), U_t(3,3), M, V_svd, U_svd, R, S, D(3,3), K(4*number_of_faces, number_of_faces+number_of_vertices), kx(4,4), identity(3,3), T_t(3,1), K_inv;
-Eigen::Vector4f q0;
-int index_1, index_2, index_3;
-std::vector<Eigen::MatrixXf> symmetric;
-std::vector<Eigen::MatrixXf> inv_kx;
-float* areas = new float [number_of_faces];
-float* quaternions = new float [4*number_of_faces];
-float* translations = new float [3*number_of_faces];
-float* angle = new float [number_of_faces];
+    Eigen::Vector3f v1, v2, v3, v4, v1_v2, v2_v3, cross_v, u1, u2, u3, u4, u1_u2, u2_u3, cross_u, T ;
+    Eigen::MatrixXf V_t(3,3), U_t(3,3), M, V_svd, U_svd, R, S, D(3,3), K(4*number_of_faces, number_of_faces+number_of_vertices), kx(4,4), identity(3,3), T_t(3,1), K_inv;
+    Eigen::Vector4f q0;
+    int index_1, index_2, index_3;
+    std::vector<Eigen::MatrixXf> symmetric;
+    std::vector<Eigen::MatrixXf> inv_kx;
+    float* areas = new float [number_of_faces];
+    float* quaternions = new float [4*number_of_faces];
+    float* translations = new float [3*number_of_faces];
+    float* angle = new float [number_of_faces];
 
-Eigen::MatrixXf S_t, Rot_t(3,3), M_t, bx(4*number_of_faces,1), by(4*number_of_faces,1), bz(4*number_of_faces,1), V_x, V_y, V_z;
-Eigen::Vector4f q, q_t;
-float w, x, y,z;
-int index;
-for (int i=0; i<4*number_of_faces; i++){
-    for (int j=0;  j<number_of_faces+number_of_vertices; j++){
-        K(i,j) = 0;
+    Eigen::MatrixXf S_t, Rot_t(3,3), M_t, bx(4*number_of_faces,1), by(4*number_of_faces,1), bz(4*number_of_faces,1), V_x, V_y, V_z;
+    Eigen::Vector4f q, q_t;
+    float w, x, y,z;
+    int index;
+    for (int i=0; i<4*number_of_faces; i++){
+        for (int j=0;  j<number_of_faces+number_of_vertices; j++){
+            K(i,j) = 0;
+        }
     }
-}
 
-for (int i=0; i<number_of_faces;i++){
-    index_1 = FV[3*i]-1, index_2 = FV[3*i+1]-1, index_3 = FV[3*i+2]-1;
-    v1 << V[3*index_1],
-    V[3*index_1+1],
-    V[3*index_1+2];
+    for (int i=0; i<number_of_faces;i++){
+        index_1 = FV[3*i]-1, index_2 = FV[3*i+1]-1, index_3 = FV[3*i+2]-1;
+        v1 << V[3*index_1],
+        V[3*index_1+1],
+        V[3*index_1+2];
 
-    v2 << V[3*index_2],
-    V[3*index_2+1],
-    V[3*index_2+2];
+        v2 << V[3*index_2],
+        V[3*index_2+1],
+        V[3*index_2+2];
 
-    v3 << V[3*index_3],
-    V[3*index_3+1],
-    V[3*index_3+2];
+        v3 << V[3*index_3],
+        V[3*index_3+1],
+        V[3*index_3+2];
 
-    v1_v2 = v2 - v1;
-    v2_v3 = v3 - v2;
-    cross_v = v1_v2.cross(v2_v3);
-    v4 = 1/3.0f*(v1+v2+v3) + cross_v*1/cross_v.norm();
+        v1_v2 = v2 - v1;
+        v2_v3 = v3 - v2;
+        cross_v = v1_v2.cross(v2_v3);
+        v4 = 1/3.0f*(v1+v2+v3) + cross_v*1/cross_v.norm();
 
 
-    areas[i] = cross_v.norm()/2.0f;
+        areas[i] = cross_v.norm()/2.0f;
 
-    V_t<<v1-v4, v2-v4, v3-v4;
+        V_t<<v1-v4, v2-v4, v3-v4;
 
-    u1 << V2[3*index_1],
-    V2[3*index_1+1],
-    V2[3*index_1+2];
+        u1 << V2[3*index_1],
+        V2[3*index_1+1],
+        V2[3*index_1+2];
 
-    u2 << V2[3*index_2],
-    V2[3*index_2+1],
-    V2[3*index_2+2];
+        u2 << V2[3*index_2],
+        V2[3*index_2+1],
+        V2[3*index_2+2];
 
-    u3 << V2[3*index_3],
-    V2[3*index_3+1],
-    V2[3*index_3+2];
+        u3 << V2[3*index_3],
+        V2[3*index_3+1],
+        V2[3*index_3+2];
 
-    u1_u2 = u2 - u1;
-    u2_u3 = u3 - u2;
+        u1_u2 = u2 - u1;
+        u2_u3 = u3 - u2;
 
-    cross_u = u1_u2.cross(u2_u3);
-    u4 = 1/3.0f*(u1+u2+u3) + cross_u*1/cross_u.norm();
+        cross_u = u1_u2.cross(u2_u3);
+        u4 = 1/3.0f*(u1+u2+u3) + cross_u*1/cross_u.norm();
 
-    U_t<< u1-u4, u2-u4, u3-u4;
+        U_t<< u1-u4, u2-u4, u3-u4;
 
-    M = U_t*V_t.inverse();
-    T = u1 - M*v1;
+        M = U_t*V_t.inverse();
+        T = u1 - M*v1;
 
-    Eigen::JacobiSVD<Eigen::MatrixXf> svd(M,  Eigen::ComputeThinU | Eigen::ComputeThinV);
-    V_svd = svd.matrixU();
-    U_svd = svd.matrixV();
-    D << svd.singularValues()[0], 0, 0,
-        0,  svd.singularValues()[1], 0,
-        0, 0, svd.singularValues()[2];
+        Eigen::JacobiSVD<Eigen::MatrixXf> svd(M,  Eigen::ComputeThinU | Eigen::ComputeThinV);
+        V_svd = svd.matrixU();
+        U_svd = svd.matrixV();
+        D << svd.singularValues()[0], 0, 0,
+            0,  svd.singularValues()[1], 0,
+            0, 0, svd.singularValues()[2];
 
-    S = U_svd*D*(U_svd.transpose());
-    R = V_svd*U_svd.transpose();
+        S = U_svd*D*(U_svd.transpose());
+        R = V_svd*U_svd.transpose();
 
-    kx << v1.transpose(), 1,
-          v2.transpose(), 1,
-          v3.transpose(), 1,
-          v4.transpose(),1;
+        kx << v1.transpose(), 1,
+            v2.transpose(), 1,
+            v3.transpose(), 1,
+            v4.transpose(),1;
 
-    w = 0.5f* sqrt(1 + R(0,0)+ R(1,1)+ R(2,2));
-    x = 1/(4*w)*(R(1,2) - R(2,1)); 
-    y = 1/(4*w)*(R(2,0) - R(0,2)); 
-    z = 1/(4*w)*(R(0,1) - R(1,0)); 
+        w = 0.5f* sqrt(1 + R(0,0)+ R(1,1)+ R(2,2));
+        x = 1/(4*w)*(R(1,2) - R(2,1)); 
+        y = 1/(4*w)*(R(2,0) - R(0,2)); 
+        z = 1/(4*w)*(R(0,1) - R(1,0)); 
 
-    quaternions[4*i]=w;
-    quaternions[4*i+1]=x;
-    quaternions[4*i+2]=y;
-    quaternions[4*i+3]=z;
+        quaternions[4*i]=w;
+        quaternions[4*i+1]=x;
+        quaternions[4*i+2]=y;
+        quaternions[4*i+3]=z;
 
-    symmetric.push_back(S);
-    inv_kx.push_back(kx.inverse());
+        symmetric.push_back(S);
+        inv_kx.push_back(kx.inverse());
 
-    translations[3*i]= T(0,0);
-    translations[3*i+1]= T(1,0);
-    translations[3*i+2] = T(2,0);
-}
+        translations[3*i]= T(0,0);
+        translations[3*i+1]= T(1,0);
+        translations[3*i+2] = T(2,0);
+    }
 
-q0 <<1, 0, 0, 0;
-identity<< 1,0,0,
-            0,1,0,
-            0,0,1;
+    q0 <<1, 0, 0, 0;
+    identity<< 1,0,0,
+                0,1,0,
+                0,0,1;
 
     for(int i=0; i<number_of_faces; i++){
         for(int j=0; j<3; j++){
@@ -313,6 +313,7 @@ identity<< 1,0,0,
                 t*translations[3*i+1],
                 t*translations[3*i+2];
 
+            q<< quaternions[4*i], quaternions[4*i+1] , quaternions[4*i+2], quaternions[4*i+3];
             q_t = 1/sin(angle[i])*(sin((1-t)*angle[i]))*q0 + sin(t*angle[i])/sin(angle[i])*q;
 
             Rot_t<< 1-2*q_t(2)*q_t(2) - 2*q_t(3)*q_t(3), 2*q_t(1)*q_t(2)+2*q_t(0)*q_t(3), 2*q_t(3)*q_t(1)- 2*q_t(0)*q_t(2),
